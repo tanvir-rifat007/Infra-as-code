802b94f3e78e02dded734fa283642786e096273a
tanvir-rifat007 <tr9836859@gmail.com>
2025-05-24 05:36:22 +0600

    Feat: added github repo automation and tfe workspace for this github repo

--- data.tf
+++ data.tf
@@ -1,3 +1,9 @@
+# data blocks are used to fetch and reference external or existing resources that are not managed by your current Terraform configuration
+
+# Here i fetch the github app installatin id for the terraform cloud 
+
+# and then use this data id in the main.tf in the vcs_repo
+
 data "tfe_github_app_installation" "this" {
   installation_id = var.github_app_installation_id
 }
--- locals.tf
+++ locals.tf
@@ -13,5 +13,16 @@ locals {
       vcs_repo_identifier = "${var.github_organization_name}/Infra-as-code"
 
     }
+
+  
+      "fem-eci-github" = {
+
+              description    = "Example automation workspace for Creating github repo."
+      execution_mode = "local"
+      project_id     = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/Infra-as-code-github"
+
+      }
+    
   }
 }
