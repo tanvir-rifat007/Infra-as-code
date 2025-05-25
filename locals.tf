a06f72e9ca415c3c9f416704e7e6352fda3f68aa
Tanvir Rifat <71336291+tanvir-rifat007@users.noreply.github.com>
2025-05-25 15:43:15 +0600

    Feature/add resources 4 (#6)
    * Feat: added aws network and cluster workspace
    
    * Fixed Bug: added variables in the main.tf file
    
    * Feat: added the product service automation
    
    * Feat: added the network2 workspace for aws
    
    * Feat: added the network2 workspace for aws
    
    * Feat: added the network2 workspace for aws

--- locals.tf
+++ locals.tf
@@ -116,11 +116,15 @@ locals {
     }
 
 
-    "fem-eci-aws-cluster-prod2" = {
+
+     
+"fem-eci-aws-cluster-prod2" = {
   description         = "Automation for AWS cluster resources2."
   execution_mode      = "remote"
   project_id          = module.project["fem-eci-project"].id
   vcs_repo_identifier = "${var.github_organization_name}/Infra-as-code-aws-cluster2"
+
+  depends_on = ["fem-eci-aws-network2"]
 
   variables = [
     {
@@ -130,6 +134,7 @@ locals {
     },
     {
       category = "terraform"
+      hcl      = true
       key      = "vpc_id"
       value    = "tfe_outputs.fem-eci-aws-network2.vpc_id"
     },
