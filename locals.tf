e7de28ba72c2bb4d7eb352f072ef14d107df03ae
tanvir-rifat007 <tr9836859@gmail.com>
2025-05-25 20:53:36 +0600

    Feat: added the network2 workspace for aws

--- locals.tf
+++ locals.tf
@@ -136,19 +136,19 @@ locals {
       category = "terraform"
       hcl      = true
       key      = "vpc_id"
-      value    =  data.terraform_remote_state.network.outputs.vpc_id
+      value    =  "data.terraform_remote_state.network.outputs.vpc_id"
     },
     {
       category = "terraform"
       hcl      = true
       key      = "subnets"
-      value    = [data.terraform_remote_state.network.outputs.private_subnets]
+      value    = "[data.terraform_remote_state.network.outputs.private_subnets]"
     },
     {
       category = "terraform"
       hcl      = true
       key      = "security_groups"
-      value    = [data.terraform_remote_state.network.outputs.private_security_group]
+      value    = "[data.terraform_remote_state.network.outputs.private_security_group]"
     },
     {
       category = "terraform"
